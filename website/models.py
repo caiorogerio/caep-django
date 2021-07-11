6554d2ce8f6689e3a0b5087b5a3b703e6387d7b3
Caio Rogerio <caiorogerio@gmail.com>
2021-07-11 02:19:32 -0300

    - ignore db - fix project name - remove WYSIWYG

--- website/models.py
+++ website/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from tinymce.models import HTMLField
+# from tinymce.models import HTMLField
 from django.utils.translation import gettext_lazy as _
 
 
@@ -7,7 +7,7 @@ class Section(models.Model):
     page = models.CharField(max_length=100)
     name = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=1024)
-    content = HTMLField()
+    content = models.TextField()
 
     def __str__(self):
         return self.title
